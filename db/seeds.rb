2dcb494062bee09c1bc33b4665e75e7508cf4244
philmarcu <philmark18@gmail.com>
2022-07-25 09:11:58 -0600

    Initial Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hr = Department.create!(name: "Human Resources", floor: "Third")
+it = Department.create!(name: "Info Technology", floor: "First")
+ad = Department.create!(name: "Marketing", floor: "Second")
+
+mark = ad.employees.create!(name: "Marky Mark", level: 2)
+jeff = it.employees.create!(name: "Jeff C", level: 1)
+dana = hr.employees.create!(name: "Dana W", level: 3)
+greg = it.employees.create!(name: "Old Greg", level: 1)
+joe = ad.employees.create!(name: "Marky Mark", level: 2)
+dale = ad.employees.create!(name: "Dale Dimmadome", level: 2)
+laney = hr.employees.create!(name: "Laney L.", level: 3)
---
+++ spec/features/departments/index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Dept index page' do
+  context 'user story 1' do
+    it 'lists all departments and employees' do
+      
+    end
+  end
+end
